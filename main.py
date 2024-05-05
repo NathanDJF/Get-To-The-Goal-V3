e5b3f39308de4b24cc7c0640ff1857d23105726c
NathanDJF <nathandjfcodes@gmail.com>
2024-05-05 20:37:56 +0700

    commit msg

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pygame
 import os
-import os
+import sys
+
+screen = pygame.display.set_mode((900, 500))
+pygame.display.set_caption('Test')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
