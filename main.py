2128b2849d6c25b99cb3656a44b9398df1f3754e
NathanDJF <nathandjfcodes@gmail.com>
2024-05-05 21:24:39 +0700

    i farted so much i farted

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import pygame
 import os
 import sys
 
-screen = pygame.display.set_mode((900, 500))
-pygame.display.set_caption('Test')
+# images
+player_img = pygame.image.load(os.path.join('Assets/player.png'))
 
+# screen
+screen = pygame.display.set_mode((900, 500))
+pygame.display.set_caption('Get To The Goal V3')
+
+# main loop
 while True:
+    screen.fill('white')
+    screen.blit(player_img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
