aac6d32b03970496178b819986b0f3ffc48c1ec5
NathanDJF <nathandjfcodes@gmail.com>
2024-06-23 22:02:27 +0700

    added the moving spike img and its collision

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 # images
 player_img = pygame.image.load(os.path.join('Assets/player.png'))
 spike_man_img = pygame.image.load(os.path.join('Assets/spike.png'))
+moving_spike_man_img = pygame.image.load(os.path.join('Assets/moving spike.png'))
 
 # screen
 screen = pygame.display.set_mode((900, 500))
@@ -12,12 +13,13 @@ pygame.display.set_caption('Get To The Goal V3')
 
 # movement
 class Player():
-    def __init__(self, x, y, vel, img, spikes):
+    def __init__(self, x, y, vel, img, spikes, moving_spikes):
         self.x = x
         self.y = y
         self.vel = vel
         self.img = img
         self.spikes = spikes
+        self.moving_spikes = moving_spikes
         
     def draw(self):
         keys = pygame.key.get_pressed()
@@ -39,18 +41,29 @@ class Player():
                 self.y += self.vel
             
         for spike in self.spikes:
-            if self.check_collision(spike):
+            if self.check_collision_spike(spike):
+                self.x = 20
+                self.y = 200
+                break
+            
+        for moving_spike in self.moving_spikes:
+            if self.check_collision_moving_spike(moving_spike):
                 self.x = 20
                 self.y = 200
                 break
         
         screen.blit(self.img, (self.x, self.y))
     
-    def check_collision(self, spike):
+    def check_collision_spike(self, spike):
         player_rect = self.img.get_rect(topleft=(self.x, self.y))
         spike_rect = spike.img.get_rect(topleft=(spike.x + 15, spike.y + 15), width=70, height=70)
         return player_rect.colliderect(spike_rect)
-        
+    
+    def check_collision_moving_spike(self, moving_spike):
+        player_rect = self.img.get_rect(topleft=(self.x, self.y))
+        moving_spike_rect = moving_spike.img.get_rect(topleft=(moving_spike.x + 15, moving_spike.y + 15), width=70, height=70)
+        return player_rect.colliderect(moving_spike_rect)
+
 class Spike():
     def __init__(self, x, y, img):
         self.x = x
@@ -60,9 +73,20 @@ class Spike():
     def draw(self):
         screen.blit(self.img, (self.x, self.y))
         
-player_thing = Player(20, 200, 5, player_img, [])
-spikes = [Spike(400, 200, spike_man_img), Spike(400, 300, spike_man_img)]
+class Moving_Spike():
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        
+    def draw(self):
+        screen.blit(self.img, (self.x, self.y))
+        
+player_thing = Player(20, 200, 5, player_img, [], [])
+spikes = [Spike(400, 200, spike_man_img)]
+moving_spikes = [Moving_Spike(700, 200, moving_spike_man_img)]
 player_thing.spikes = spikes
+player_thing.moving_spikes = moving_spikes
 
 # main loop
 while True:
@@ -72,6 +96,8 @@ while True:
     player_thing.draw()
     for spike in spikes:
         spike.draw()
+    for moving_spike in moving_spikes:
+        moving_spike.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
