f1e14c3a6036164a1cfa93aa760fa2b5e583b935
NathanDJF <nathandjfcodes@gmail.com>
2024-06-26 18:03:13 +0700

    level 2 and 3

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ font3 = pygame.font.SysFont("Arial", 45, bold=False, italic=False)
 # some variables or smth
 completed_level = False
 current_level = 1
-winning_text = font3.render("Level Complete! Press ENTER to go to the next level", True, (0, 0, 0))
+winning_text = font3.render("Level Complete! Press ENTER to go to the next level", True, (255, 255, 255))
 
 # movement
 class Player():
@@ -73,14 +73,6 @@ class Player():
         
         if self.check_collision_flag(self.flag):
             completed_level = True
-            
-        if completed_level:
-            screen.blit(winning_text, (15, 200))
-            if keys[pygame.K_RETURN]:
-                self.x = 20
-                self.y = 200
-                current_level += 1
-                completed_level = False
         
         screen.blit(self.img, (self.x, self.y))
     
@@ -98,6 +90,18 @@ class Player():
         player_rect = self.img.get_rect(topleft=(self.x, self.y))
         flag_rect = flag.img.get_rect(topleft=(flag.x + 50, flag.y + 50), width=1, height=100)
         return player_rect.colliderect(flag_rect)
+    
+    def check(self):
+        global completed_level
+        global current_level
+        keys = pygame.key.get_pressed()
+        if completed_level:
+            screen.blit(winning_text, (15, 200))
+            if keys[pygame.K_RETURN]:
+                self.x = 20
+                self.y = 200
+                current_level += 1
+                completed_level = False
 
 class Spike():
     def __init__(self, x, y, img):
@@ -155,8 +159,7 @@ class Level():
     
     def run(self):
         if self.level == 1:
-            self.flag.draw()
-            player_thing.draw()
+            # text
             level_1_text = font.render("Level 1", True, (0, 0, 0))
             level_1_description_1 = font2.render("Welcome to Gttg v3", True, (0, 0, 0))
             level_1_description_2 = font2.render("You move with WASD or arrow keys", True, (0, 0, 0))
@@ -165,37 +168,77 @@ class Level():
             screen.blit(level_1_description_1, (50, 50))
             screen.blit(level_1_description_2, (50, 75))
             screen.blit(level_1_description_3, (50, 100))
+            
+            # the level
+            player_thing.draw()
+            self.flag.draw()
+            
+            player_thing.check()
 
         if self.level == 2:
-            self.flag.draw()
+            # the player flag
             player_thing.draw()
+            self.flag.draw()
+            
+            # spikes
+            spikes = [Spike(400, 200, spike_man_img)]
+            player_thing.spikes = spikes
+            for spike in spikes:
+                spike.draw()
+                
+            player_thing.check()
+            
+            # text
             level_2_text = font.render("Level 2", True, (0, 0, 0))
+            level_2_description_1 = font2.render("Don't touch the spike", True, (0, 0, 0))
+            level_2_description_2 = font2.render("I think it kills you", True, (0, 0, 0))
             screen.blit(level_2_text, (400, 0))
+            screen.blit(level_2_description_1, (50, 50))
+            screen.blit(level_2_description_2, (50, 75))
+            
+        if self.level == 3:
+            # the player flag
+            player_thing.draw()
+            self.flag.draw()
+            
+            # spikes
+            spikes = [Spike(350, 200, spike_man_img), Spike(450, 200, spike_man_img)]
+            player_thing.spikes = spikes
+            for spike in spikes:
+                spike.draw()
+                
+            player_thing.check()
+            
+            # text
+            level_3_text = font.render("Level 3", True, (0, 0, 0))
+            level_3_description = font2.render("More spikes", True, (0, 0, 0))
+            screen.blit(level_3_text, (400, 0))
+            screen.blit(level_3_description, (50, 50))
         
 flag = Flag(775, 300, flag_img)
 player_thing = Player(20, 200, 5, player_img, [], [], flag)
-#spikes = [Spike(400, 200, spike_man_img)]
 #moving_spikes = [Moving_Spike(700, 200, moving_spike_man_img, 4), Moving_Spike(800, 200, moving_spike_man_img, 100)]
-#player_thing.spikes = spikes
 #player_thing.moving_spikes = moving_spikes
 
 # level stuff
 level_1 = Level(1, flag)
 level_2 = Level(2, flag)
+level_3 = Level(3, flag)
 
 # main loop
 while True:
     clock = pygame.time.Clock()
     clock.tick(60)
-    screen.fill('cyan')
-    #for spike in spikes:
-        #spike.draw()
+    screen.fill((0, 128, 255))
     #for moving_spike in moving_spikes:
         #moving_spike.draw()
     if current_level == 1:
         level_1.run()
     elif current_level == 2:
         level_2.run()
+    elif current_level == 3:
+        level_3.run()
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
